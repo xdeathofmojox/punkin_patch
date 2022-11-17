7f78973ff41e39b65d03886159fe7e914b9457b8
xdeathofmojox <xdeathofmojox@gmail.com>
2022-11-16 20:15:59 -0500

    Update CreateCharacterForm.js
    Refactor CreateCharacterForm to be a function instead of using the full class implementation

--- src/Components/CreateCharacterForm.js
+++ src/Components/CreateCharacterForm.js
@@ -1,96 +1,96 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class CreateCharacterForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: "",
-        myers_briggs: "entp",
-        enneagram: "", 
-        media: ""
-      };
-  
-      this.handleInputChange = this.handleInputChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleInputChange(event) {
-      const target = event.target;
-      const value = target.type === 'checkbox' ? target.checked : target.value;
-      const name = target.name;
-  
-      this.setState({
-        [name]: value
-      });
-    }
+function CreateCharacterForm() {
 
-    handleSubmit(event) {
-        console.log(this.state.name);
-        console.log(this.state.myers_briggs);
-        console.log(this.state.enneagram);
-        console.log(this.state.media);
-        event.preventDefault();
+    const [characterName, setCharacterName] = useState("");
+    const [myersBriggs, setMyersBriggs] = useState("entp");
+    const [enneagram, setEnneagram] = useState("");
+    const [media, setMedia] = useState("");
+  
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await fetch("https://httpbin.org/post", {
+                method: "POST",
+                body: JSON.stringify({
+                        characterName: characterName,
+                        myersBriggs: myersBriggs,
+                        enneagram: enneagram,
+                        media: media,
+                }),
+            });
+            await res.json();
+            if (res.status === 200) {
+                setCharacterName("");
+                setMyersBriggs("entp");
+                setEnneagram("");
+                setMedia("");
+                alert("User created successfully");
+            } else {
+                alert("Some error occured");
+            }
+        } catch (err) {
+            console.log(err);
+        }
     }
   
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name
-            <input
-              name="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Media
-            <input
-              name="media"
-              type="text"
-              value={this.state.media}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Myers Briggs:
-            <select
-              name="myers_briggs"
-              value={this.state.myers_briggs}
-              onChange={this.handleInputChange}>
-                <option value="istj">ISTJ</option>
-                <option value="isfj">ISFJ</option>
-                <option value="infj">INFJ</option>
-                <option value="intj">INTJ</option>
-                <option value="istp">ISTP</option>
-                <option value="isfp">ISFP</option>
-                <option value="infp">INFP</option>
-                <option value="intp">INTP</option>
-                <option value="estj">ESTJ</option>
-                <option value="esfj">ESFJ</option>
-                <option value="enfj">ENFJ</option>
-                <option value="entj">ENTJ</option>
-                <option value="estp">ESTP</option>
-                <option value="esfp">ESFP</option>
-                <option value="enfp">ENFP</option>
-                <option value="entp">ENTP</option>
-            </select>
-          </label>
-          <br/>
-          <label>
-            Enneagram
-            <input
-              name="enneagram"
-              type="text"
-              value={this.state.enneagram}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <input type="submit" value="Create" />
-        </form>
-      );
-    }
+    return (
+    <form onSubmit={handleSubmit}>
+        <label>
+        Name
+        <input
+            name="characterName"
+            type="text"
+            value={characterName}
+            onChange={(e) => setCharacterName(e.target.value)} />
+        </label>
+        <br />
+        <label>
+        Media
+        <input
+            name="media"
+            type="text"
+            value={media}
+            onChange={(e) => setMedia(e.target.value)} />
+        </label>
+        <br />
+        <label>
+        Myers Briggs:
+        <select
+            name="myersBriggs"
+            value={myersBriggs}
+            onChange={(e) => setMyersBriggs(e.target.value)}>
+            <option value="istj">ISTJ</option>
+            <option value="isfj">ISFJ</option>
+            <option value="infj">INFJ</option>
+            <option value="intj">INTJ</option>
+            <option value="istp">ISTP</option>
+            <option value="isfp">ISFP</option>
+            <option value="infp">INFP</option>
+            <option value="intp">INTP</option>
+            <option value="estj">ESTJ</option>
+            <option value="esfj">ESFJ</option>
+            <option value="enfj">ENFJ</option>
+            <option value="entj">ENTJ</option>
+            <option value="estp">ESTP</option>
+            <option value="esfp">ESFP</option>
+            <option value="enfp">ENFP</option>
+            <option value="entp">ENTP</option>
+        </select>
+        </label>
+        <br/>
+        <label>
+        Enneagram
+        <input
+            name="enneagram"
+            type="text"
+            value={enneagram}
+            onChange={(e) => setEnneagram(e.target.value)} />
+        </label>
+        <br />
+        <input type="submit" value="Create" />
+    </form>
+    );
   }
 
   export default CreateCharacterForm;
