b0d9bc5179f97edc605d91c9123322e158e3a91c
xdeathofmojox <xdeathofmojox@gmail.com>
2022-11-17 17:45:19 -0500

    Populate characters with temp data

---
+++ src/Components/CharacterCard.js
@@ -0,0 +1,9 @@
+function CharacterCard(props) {
+    return (
+        <div>
+            <h1>ID: {props.id}</h1>
+        </div>
+    );
+}
+
+export default CharacterCard;
--- src/Components/ViewCharactersPage.js
+++ src/Components/ViewCharactersPage.js
@@ -1,8 +1,17 @@
+import CharacterCard from "./CharacterCard"
+
 function ViewCharactersPage()
 {
+    const characters = [{value: 1, label: "Ichigo"}, {value: 2, label: "Soap"}, {value: 3, label: "Sephiroth"}, {value: 4, label: "Yoruichi"}, {value: 5, label: "Aizawa"}];
+
     return (
         <div>
             <h1>View Characters Page</h1>
+            {characters.map((element, index) => (
+                <div className="form-inline" key={index}>
+                    <CharacterCard id={element.value} />
+                </div>
+            ))}
         </div>
     );
 }
